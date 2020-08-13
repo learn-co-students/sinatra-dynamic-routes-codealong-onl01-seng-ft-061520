2849a77d62787a557c47c2ad03db8fabc89fddc7
devWRM <mcwen@comcast.net>
2020-08-13 14:03:05 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
 
   # This is a sample static route.
   get '/hello' do
-    "Hello World!"
+    "Hello My World!"
   end
 
   # This is a sample dynamic route.
@@ -14,5 +14,19 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+
+  # get '/multiply/:num1/:num2' do
+  #   "Hello World"
+  # end
+
+  get '/multiply/:num1/:num2' do
+# binding.pry
+    "#{params[:num1].to_i * params[:num2].to_i}"
+  end
 
 end
